be00e3fedc7a88fee4150276f653809f8dac70d5
JankoV12 <drobilek.jan@gmail.com>
2024-09-11 11:34:36 +0200

    Initial Commit

---
+++ DOMJudge/DOMJudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DOMJudgeApp.swift
+//  DOMJudge
+//
+//  Created by Jan Drobílek on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct DOMJudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DOMJudgeTests/DOMJudgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  DOMJudgeTests.swift
+//  DOMJudgeTests
+//
+//  Created by Jan Drobílek on 11.09.2024.
+//
+
+import Testing
+@testable import DOMJudge
+
+struct DOMJudgeTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
